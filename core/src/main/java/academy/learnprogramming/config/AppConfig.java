b28578683d729d331c7e83a8a74f0c1fe9a0edfb
Rafael Brandao <r.augusto.brandao@gmail.com>
2019-12-19 14:28:27 -0500

    Created GameConfig class and added it to the Configuration classes loaded on AppConfig

--- core/src/main/java/academy/learnprogramming/config/AppConfig.java
+++ core/src/main/java/academy/learnprogramming/config/AppConfig.java
@@ -5,8 +5,10 @@ import academy.learnprogramming.MessageGeneratorImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(GameConfig.class)
 @ComponentScan(basePackages = "academy.learnprogramming")
 public class AppConfig {
 
---
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -0,0 +1,7 @@
+package academy.learnprogramming.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GameConfig {
+}
