fe7414b9a92f82851a077a5f62d536412dd7c862
Rafael Brandao <r.augusto.brandao@gmail.com>
2019-12-19 14:30:34 -0500

    Centralized variables maxNumber and guessCount on the GameConfig class

--- core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -1,7 +1,21 @@
 package academy.learnprogramming.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GameConfig {
+
+    private int maxNumber = 25;
+    private int guessCount = 5;
+
+    @Bean
+    public int maxNumber(){
+        return maxNumber;
+    }
+
+    @Bean
+    public int guessCount(){
+        return guessCount;
+    }
 }
