d2e2fabb160bf5a1092aea3f0b2b5128d37f0e32
Rafael Brandao <r.augusto.brandao@gmail.com>
2019-12-27 16:25:44 -0500

    Changed use of @Bean to @Component on MessageGeneratorImpl class

--- core/src/main/java/academy/learnprogramming/config/AppConfig.java
+++ core/src/main/java/academy/learnprogramming/config/AppConfig.java
@@ -1,8 +1,5 @@
 package academy.learnprogramming.config;
 
-import academy.learnprogramming.MessageGenerator;
-import academy.learnprogramming.MessageGeneratorImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,9 +8,4 @@ import org.springframework.context.annotation.Import;
 @Import(GameConfig.class)
 @ComponentScan(basePackages = "academy.learnprogramming")
 public class AppConfig {
-
-    @Bean
-    public MessageGenerator messageGenerator(){
-        return new MessageGeneratorImpl();
-    }
 }
