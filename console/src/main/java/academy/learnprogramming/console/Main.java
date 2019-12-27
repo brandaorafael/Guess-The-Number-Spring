6adc8ddfe97d70db1013b5d1ac11f0b027743fb9
Rafael Brandao <r.augusto.brandao@gmail.com>
2019-12-27 16:21:50 -0500

    Removed unused code in Main Class

--- console/src/main/java/academy/learnprogramming/console/Main.java
+++ console/src/main/java/academy/learnprogramming/console/Main.java
@@ -1,8 +1,6 @@
 package academy.learnprogramming.console;
 
 import academy.learnprogramming.config.AppConfig;
-import academy.learnprogramming.MessageGenerator;
-import academy.learnprogramming.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,21 +16,6 @@ public class Main {
         // Create Context (Container)
         ConfigurableApplicationContext context
                 = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        // Get number generator bean from the context (container)
-        NumberGenerator numberGenerator
-                = context.getBean(NumberGenerator.class);
-
-        // call method next() to get a random number
-        int number = numberGenerator.next();
-
-        // log generated number
-        log.info("number = {}", number);
-
-        // get the MessageGenerator Bean from context (container)
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-        log.info(messageGenerator.getMainMessage());
-        log.info(messageGenerator.getResultMessage());
 
         // close context
         context.close();
