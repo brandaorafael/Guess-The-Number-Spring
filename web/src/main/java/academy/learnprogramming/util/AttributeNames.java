c045b26338be567bfa9aa866b63a7fedd9b36371
Rafael Brandao <r.augusto.brandao@gmail.com>
2020-01-06 13:55:47 -0500

    Created AttributeNames and GameMappings classes

---
+++ web/src/main/java/academy/learnprogramming/util/AttributeNames.java
@@ -0,0 +1,9 @@
+package academy.learnprogramming.util;
+
+public final class AttributeNames {
+
+    private AttributeNames(){}
+
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+}
---
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class GameMappings {
+
+    private GameMappings() {}
+
+    public static final String PLAY = "play";
+    public static final String RESTART = "reset";
+    public static final String HOME = "/";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+}
