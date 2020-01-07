41afbf40c26a41983f2ec2885a5a899dc8104076
Rafael Brandao <r.augusto.brandao@gmail.com>
2020-01-07 13:58:47 -0500

    Created a RequestInterceptor to intercept the request and also implemented a LocaleInterceptor

--- web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,15 +1,37 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.GameMappings;
 import academy.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController(GameMappings.HOME).setViewName(ViewNames.HOME);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
+
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang"); // Defult is locale
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+
 }
